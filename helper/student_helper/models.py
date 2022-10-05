15a909c0bd0667a2f5d33d79f1e0ad7cc515947f
maria <mary1voight@gmail.com>
2022-10-05 20:28:27 +0300

    Can create tasks now

--- helper/student_helper/models.py
+++ helper/student_helper/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Task(models.Model):
+ # """Task model."""
+
+  title = models.CharField(max_length=128, null=False)
+  description = models.TextField(null=True)
+  subject = models.TextField(null=True)
+  is_complete = models.BooleanField(default=False)
+  user = models.ForeignKey(                           #used to define one-to-many relashionships
+    settings.AUTH_USER_MODEL,
+    on_delete=models.CASCADE,
+    related_name="task",
+    null=True,
+    blank=True
+  )
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  deadline = models.DateTimeField()
+
+  def __str__(self):
+    return f"{self.user.first_name} - {self.title}"
--- helper/student_helper/views.py
+++ helper/student_helper/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters
 
-# Create your views here.
+from .models import Task
+from .serializers import TasksSerializer
+
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for Tasks.
+    """
+
+    queryset = Task.objects.all()
+    serializer_class = TasksSerializer
+    filter_backends = [
+      DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter,
+    ]
+    filterset_fields = ("title", "user", "is_complete")
+    search_fields = ("title", "subject")
+    ordering_fields = ("is_complete", "created_at", "updated_at", "deadline")
